74556718b93438f4daa58da028e06d143ab41dd3
Amonsta <amana.ammi@gmail.com>
2020-10-10 21:57:05 -0600

    add crud to product controller

--- backend-api/app/controllers/products_controller.rb
+++ backend-api/app/controllers/products_controller.rb
@@ -1,2 +1,33 @@
 class ProductsController < ApplicationController
+
+    def index
+        products = Product.all
+        render json: products
+    end
+
+    def show
+        product = Product.find(params[:id])
+        render json: product
+    end
+
+    def create
+        product = Product.create(product_params)
+        render json: produt
+    end
+
+    def update
+        product = Product.find(params[:id])
+    end
+
+    def destroy
+        product = Product.find(params[:id])
+        product.destroy
+    end
+
+    private 
+    
+    def product_params
+        params.require(:product).permit(:name, :image, :quantity, :price)
+    end
+
 end
