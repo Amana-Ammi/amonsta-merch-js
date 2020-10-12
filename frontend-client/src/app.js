1e1b7f80009b4c0e740c2e5b88ef3b60cbbfc1f4
Amonsta <amana.ammi@gmail.com>
2020-10-11 22:23:32 -0600

    successful base fetch of products from api to js

--- frontend-client/src/app.js
+++ frontend-client/src/app.js
@@ -1,3 +1,15 @@
 class App {
-    
+    products = []
+    url = "http://localhost:3000"
+
+
+    getProducts() {
+        fetch(this.url + "/products")
+        .then(resp => resp.json())
+        .then(data => console.log(data));
+    }
+
+    renderProducts() {
+
+    }
 }
