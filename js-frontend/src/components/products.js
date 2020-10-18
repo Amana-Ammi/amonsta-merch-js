fbbf8023cf325f5833b4f60a54ce8a5aa1472294
Amonsta <amana.ammi@gmail.com>
2020-10-17 19:13:59 -0600

    move files to index.js

--- js-frontend/src/components/products.js
+++ js-frontend/src/components/products.js
@@ -1,31 +1,3 @@
 class Products {
-    constructor() {
-        this.products = []
-        this.adapter = new ProductsAdapter()
-        this.fetchAndLoadProducts()
-    }
-
-    fetchAndLoadProducts() {
-        this.adapter
-        .getProducts()
-        .then(products => {
-            products.forEach(product => this.products.push(product))
-        })
-        .then(() => {
-            this.renderProducts()
-            // this.renderProductsImage()
-        })
-    }
-
-    renderProducts() {
-        // const productArray = 
-        // console.log(productArray)
-          const photobox = document.getElementById('photo-box')
-        photobox.innerHTML = this.products.map(product => `<li>${product.name}</li>`).join(' ')
-    }
-
-    // renderProductsImage() {
-    //     const img = document.getElementById('shirts')
-    //     img.innerHTML = this.products.map(product => `<li>${product.image_url}</li>`).join(' ')
-    // }
+    
 }
--- js-frontend/src/index.js
+++ js-frontend/src/index.js
@@ -1,2 +1,31 @@
-const app = new App()
-// app.getProducts()
+const baseUrl = 'http://localhost:3000/api/v1/products';
+
+document.addEventListener('DOMContentLoaded', () => {
+    getProducts()
+})
+
+function getProducts() {
+    fetch(baseUrl)
+    .then(resp => resp.json())
+    .then(products => {
+        products.data.forEach(product => {
+            const productMarkup = `
+            <article class="product">
+            <div data-id=${product.id}>
+                <img src=${product.attributes.image_url} alt="product"
+                class="product-img"/>
+                <button class="bag-btn" data-id=${product.id}>
+                    <i class="fa-cart-arrow-down"></i>
+                    add to bag
+                </button>
+            </div>
+            <h3> ${product.attributes.name} </h3>
+            <h4> $${product.attributes.price} </h4>
+        </article>
+            ` ;
+            document.querySelector('#img-container').innerHTML += productMarkup
+        })
+    })
+}
+
+
