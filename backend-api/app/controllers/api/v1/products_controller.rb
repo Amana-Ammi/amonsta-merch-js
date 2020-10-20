2735cb3b19f142624da4625783b9629693f102a7
Amonsta <amana.ammi@gmail.com>
2020-10-19 18:09:28 -0600

    dry controller actions

--- backend-api/app/controllers/api/v1/products_controller.rb
+++ backend-api/app/controllers/api/v1/products_controller.rb
@@ -1,28 +1,30 @@
 class Api::V1::ProductsController < ApplicationController
 
+    before_action :find_product, only: [:update, :destroy]
+
+
     def index
         products = Product.all
         render json: ProductSerializer.new(products).serialized_json
     end
     
-    def show
-        product = Product.find(params[:id])
-        render json: ProductSerializer.new(product).serialized_json
-    end
+    # def show
+    #     product = Product.find(params[:id])
+    #     render json: ProductSerializer.new(product).serialized_json
+    # end
 
     def create
         product = Product.new(product_params)
         if product.save
-            render json: product
+            render json: ProductSerializer.new(products).serialized_json
         end
     end
 
-    def update
-        product = Product.find(params[:id])
-    end
+    # def update
+    #     product = Product.find(params[:id])
+    # end
 
     def destroy
-        product = Product.find(params[:id])
         product.destroy
     end
 
@@ -33,4 +35,9 @@ class Api::V1::ProductsController < ApplicationController
     def product_params
         params.require(:product).permit(:name, :image_url, :price, :quantity, :category_id)
     end
+
+    def find_product
+        product = Product.find(params[:id])
+    end
+
 end
