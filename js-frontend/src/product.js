b28b3e2fbc9646ab0910050d4c16fbab92d6b586
Amonsta <amana.ammi@gmail.com>
2020-10-21 00:49:33 -0600

    adjust spacing

--- js-frontend/src/product.js
+++ js-frontend/src/product.js
@@ -6,24 +6,21 @@ class Product {
       this.price = productAttributes.price;
       this.quantity = productAttributes.quantity;
       this.category = productAttributes.category;
-      console.log('this is working');
    } 
   
 
    renderProducts() {
       return `
-      <div data-id=${this.id}>
-         <br><br><img src=${this.image_url} alt="product"
-         class="product-img"/>
-         <h3> ${this.name} </h3>
-         <h4> $${this.price} </h4><br><br>
-         <div>
-            <i class="far fa-minus-square"></i>
-            <p class="item-amount">${this.quantity}</p>
-            <i class="far fa-plus-square"></i><br><br>
-         </div>
-      <button type="submit">UPDATE</button><br><br>
-   </div>
+      <div class="prod-box" data-id=${this.id}>
+         <img src=${this.image_url} alt="product"/>
+         <h3 id="title"> ${this.name} </h3>
+         <h4> $${this.price} </h4>
+         <h4 class="item-amount"> QUANTITY: ${this.quantity} </h4>    
+   </div><br><br>
       `
    }
+
+
+
+
 }
