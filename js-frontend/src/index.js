6e2a6c5bb4c20ad750d5b67411a01ae66a8fccdd
Amonsta <amana.ammi@gmail.com>
2020-10-21 00:47:18 -0600

    add post function and event listener

--- js-frontend/src/index.js
+++ js-frontend/src/index.js
@@ -1,26 +1,16 @@
-const baseUrl = 'http://localhost:3000/api/v1/products';
-const selectElement = document.querySelector('.categories');
+const baseUrl = 'http://localhost:3000/api/v1';
 
 document.addEventListener('DOMContentLoaded', () => {
-    getProducts()
+    getAllProducts();
     
-})
+    const createProductsForm = document.querySelector('#add-product');
 
-
-selectElement.addEventListener('change', (event) => {
-    const result = document.querySelector('.result');
-    console.log('loaded')
-    // want to filter results with select tag 
-    // currently listening.need a function to filter results
+    createProductsForm.addEventListener("submit", (e) => formHandler(e))
+    
 });
 
-document.addEventListener('submit', () => {
-    console.log('update btn hit')
-// want to update products
-});
-
-function getProducts() {
-    fetch(baseUrl)
+function getAllProducts() {
+    fetch(baseUrl + '/products')
     .then(resp => resp.json())
     .then(products => {
         products.data.forEach(product => {
@@ -30,29 +20,33 @@ function getProducts() {
     })
 }
 
-//     function getProductsByCategory() {
-//         fetch(baseUrl)
-//         .then(resp => resp.json())
-//         .then(categories => {
-//             categories.data.forEach(categories => {
-//                 const productMarkup = `
-//                 <div class="child" data-id=${product.id}><br><br>
-//                     <img src=${product.attributes.image_url} alt="product"
-//                     class="prod-image"/>
-//                     <h3> ${product.attributes.name} </h3>
-//                     <h4> $${product.attributes.price} </h4>
-//                         <div class="quantity">
-//                             <i class="far fa-minus-square"></i>
-//                             ${product.attributes.quantity}
-//                             <i class="far fa-plus-square"></i><br><br>
-//                         </div>
-//                     <button type="submit">UPDATE</button><br><br>
-//                 </div><br><br>
-//                 ` ;
-//                 document.querySelector('#img-container').innerHTML += productMarkup
-//             })
-//         })
-//     }
-// }
+function formHandler(e) {
+    e.preventDefault()
+    // Form variables
+    const fName = document.querySelector("#fname").value 
+    const fImage = document.querySelector("#fimage").value
+    const fPrice = document.querySelector("#fprice").value
+    const fQuantity = document.querySelector("#fquantity").value
+    const fCategories = document.querySelector("#fcategories").value
+    postProduct(fName, fImage, fPrice, fQuantity, fCategories)
+}
+
+function postProduct(name, image_url, price, quantity, category_id) {
+    const formData = {name, image_url, price, quantity, category_id}
+    fetch(baseUrl + '/products', {
+    method: "POST",
+    headers: {"Content-Type": "application/json",
+            "Accept": "application/json"},
+    body: JSON.stringify(formData)
+    })
+    .then(resp => resp.json())
+    .then(product => {
+        const productData = product.data
+        let newProduct = new Product(productData, productData.attributes) 
+            document.querySelector('#img-container').innerHTML += newProduct.renderProducts()
+    })
+}
+
+
 
 
